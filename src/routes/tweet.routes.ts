2934ca597f10ad7b863381e80ca383fa5695d9e0
akmadan <akmadan1717@gmail.com>
2024-01-30 20:59:08 +0530

    init

---
+++ src/routes/tweet.routes.ts
@@ -0,0 +1,15 @@
+import { Router } from "express";
+import { getTweetController, createTweetController, updateTweetController, deleteTweetController } from "../controllers/tweet.controller";
+
+const tweetRouter = Router();
+
+
+// Define route paths
+
+tweetRouter.get("/:tweetId", getTweetController)
+// tweetRouter.get("/", getAllTweetsController)
+tweetRouter.post("/", createTweetController)
+tweetRouter.delete("/:userId", deleteTweetController)
+tweetRouter.put("/", updateTweetController)
+
+export default tweetRouter
